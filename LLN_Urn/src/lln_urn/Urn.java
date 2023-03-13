5c8812b8493919ed3545fe502f30ef6dc9e04a9f
apponio <113506906+apponio@users.noreply.github.com>
2023-03-14 00:10:05 +0100

    Initial commit

---
+++ LLN_Urn/src/lln_urn/Urn.java
@@ -0,0 +1,16 @@
+package lln_urn;
+
+/**
+ *
+ * @author Apponio
+ */
+public class Urn {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+        
+}
